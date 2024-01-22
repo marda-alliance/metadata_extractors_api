bedaa92868f4f8525db2b5a893e4bff839c3a1c1
PeterKraus <PeterKraus@users.noreply.github.com>
2024-01-22 12:46:34 +0000

    deploy: 4a02ac47c2762bea30ad3b12d8df1d8527642e76

--- versions.js
+++
@@ -1,8 +0,0 @@
-arr = window.location.href.split('/');
-page = arr[arr.length - 1];
-document.write('\
-<dl>\
-    <dt>Versions</dt> \
-    <dd><a href="../main/' + page + '">main</a></dd>\
-</dl>\
-');
